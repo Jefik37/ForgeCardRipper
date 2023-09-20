797871f61c9575618eb3e420b52117ae55350ff2
Jefik37 <58911866+Jefik37@users.noreply.github.com>
2023-09-20 02:05:51 -0300

    Update main.py

--- main.py
+++ main.py
@@ -3,8 +3,11 @@ import pyautogui
 import time
 from PIL import Image
 import csv
-from pynput import mouse
+from pynput.mouse import Listener as MouseListener
+from pynput.keyboard import Listener as KeyboardListener
+import pynput
 import os
+from unidecode import unidecode
 
 path_ = "C:\\Program Files\\Tesseract-OCR\\tesseract.exe"
 
@@ -12,27 +15,37 @@ pytesseract.tesseract_cmd=path_
 
 four_k=False
 
+if four_k:
+    t_w_i, t_h_i, t_w_f, t_h_f = 2626, 108, 3825, 172
+
+    s_w_i, s_h_i, s_w_f, s_h_f = 3748, 230, 3823, 281
+
+else:
+    t_w_i, t_h_i, t_w_f, t_h_f = 1313, 54, 1912, 85
+
+    s_w_i, s_h_i, s_w_f, s_h_f = 1876, 115, 1911, 140
+
 def translate(title_):
     with open('names.csv', newline='', encoding='utf8') as f:
         reader = csv.reader(f, delimiter='|')
         for i in reader:
-            if i[1] == title_:
+            if unidecode(i[1]) == unidecode(title_):
                 return i[0]
     return title_
 
+def removedash(text):
+    x=-1
+    for i in range(len(text)):
+        if text[i]=='-':
+            x=i
+    text=text[0:x].strip()
+    return text
+
 def save_card():
     if four_k:
-        t_w_i, t_h_i, t_w_f, t_h_f = 2626, 108, 3825, 172
-
-        s_w_i, s_h_i, s_w_f, s_h_f = 3748, 230, 3823, 281
-
         c_w_i, c_h_i, c_w_f, c_h_f = 1432, 380, 2407, 1739
 
     else:
-        t_w_i, t_h_i, t_w_f, t_h_f = 1313, 54, 1912, 85
-
-        s_w_i, s_h_i, s_w_f, s_h_f = 1876, 115, 1911, 140
-
         c_w_i, c_h_i, c_w_f, c_h_f = 716, 190, 1203, 869
 
     title=pyautogui.screenshot('title.jpg', region=(t_w_i, t_h_i, t_w_f-t_w_i, t_h_f-t_h_i))
@@ -51,11 +64,7 @@ def save_card():
             c_w_i, c_h_i, c_w_f, c_h_f = 620, 286, 1299, 773
         double_card=True
 
-    x=-1
-    for i in range(len(title)):
-        if title[i]=='-':
-            x=i
-    title=title[0:x].strip()
+    title=removedash(title)
 
     print(title, f'({set_})', end=' - ')
 
@@ -90,9 +99,47 @@ def on_click(x, y, button, pressed):
     if pressed and str(button)=='Button.middle':
         save_card()
 
-# def on_scroll(x, y, dx, dy):
-#     if dy==1:
-#         save_card()
 
-with mouse.Listener(on_click=on_click) as listener:
-    listener.join()
+if four_k:
+    card_location=(3190,1570)
+else:
+    card_location=(1576,790)
+
+toggle=False
+def on_press(key):
+    if not toggle and str(key)=='Key.ctrl_l':
+        globals()['toggle']=True
+        while True:
+
+            pyautogui.click(card_location)
+            pyautogui.scroll(clicks=1000)
+            save_card()
+            pyautogui.scroll(clicks=-1000)
+            pyautogui.click(card_location)
+            pyautogui.scroll(clicks=1000)
+            save_card()
+            pyautogui.scroll(clicks=-1000)
+            pyautogui.press('down')
+            time.sleep(0.5)
+
+            title=pytesseract.image_to_string('title.jpg', config='--psm 7', lang="por")
+            new_title=pyautogui.screenshot(region=(t_w_i, t_h_i, t_w_f-t_w_i, t_h_f-t_h_i))
+            new_title=pytesseract.image_to_string(new_title, config='--psm 7', lang="por")
+
+            title = removedash(title)
+            new_title = removedash(new_title)
+            # Checks if there are any new images
+            if title==new_title:
+                break
+
+def on_release(key):
+    if toggle:
+        globals()['toggle']=False
+
+keyboard_listener = KeyboardListener(on_press=on_press, on_release=on_release)
+mouse_listener = MouseListener(on_click=on_click)
+
+keyboard_listener.start()
+mouse_listener.start()
+keyboard_listener.join()
+mouse_listener.join()
